49f6856c322ffd6bca84fecdccdb6f8c58a92881
mvidal <mvidal@agendize.com>
2025-02-10 12:27:38 +0100

    Changement db postgres connexion

--- config/db.js
+++ config/db.js
@@ -1,17 +1,29 @@
-import mysql from 'mysql2';
+import pg from "pg";
+const { Pool } = pg;
 import dotenv from 'dotenv';
 
 // dotenv.config({ path: '.env.local' });
 dotenv.config();
 
-const pool = mysql.createPool({
+const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-export default pool.promise();
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Erreur de connexion à la base de données :', err);
+    } else {
+        console.log('Connexion réussie !', res.rows);
+    }
+});
+
+export default pool;
